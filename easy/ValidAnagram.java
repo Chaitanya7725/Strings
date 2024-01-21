0a9a60d1a71e251657fc79f060f88e3d33966b69
Chaitanya7725 <chaitanya7725@gmail.com>
2024-01-20 23:30:50 -0600

    Hashmap for characters count

--- easy/ValidAnagram.java
+++ easy/ValidAnagram.java
@@ -1,6 +1,7 @@
 package easy;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ValidAnagram {
 
@@ -11,17 +12,17 @@ public class ValidAnagram {
     }
 
     private static boolean check(String first, String second) {
-        if (first.length()!=second.length()) return false;
+        Map<Character,Integer> archive1 = new HashMap<>();
+        Map<Character,Integer> archive2 = new HashMap<>();
+        if (first.length() == second.length()){
+            for (int i = 0; i < first.length(); i++) {
+                archive1.put(first.charAt(i),archive1.getOrDefault(archive1.get(first.charAt(i)),0)+1);
+                archive2.put(second.charAt(i),archive2.getOrDefault(archive2.get(second.charAt(i)),0)+1);
+            }
+            if(archive2.equals(archive1)) return true;
+        }
 
-        first = first.toLowerCase();
-        second = second.toLowerCase();
-        char f []=first.toCharArray();
-        Arrays.sort(f);
-        first=String.valueOf(f);
-        char [] s=second.toCharArray();
-        Arrays.sort(s);
-        second=String.valueOf(s);
-        return first.equals(second);
+        return false;
     }
 
 }
