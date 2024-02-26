552c63a904de2baa6cd8150f0c6a6a95d6193d55
Chaitanya7725 <chaitanya7725@gmail.com>
2024-02-25 19:43:46 -0600

    Generating valid parenthesis based on input number as a List of StringBuilder

--- medium/GenerateParenthesis.java
+++ medium/GenerateParenthesis.java
@@ -11,17 +11,23 @@ public class GenerateParenthesis {
 
     public static List<String> generateParenthesis(int n) {
         List<String> output_array= new ArrayList<>();
-        backtrack(output_array,"", 0,0,n);
+        backtrack(output_array,new StringBuilder(), 0,0,n);
         return output_array;
     }
 
-    public static void backtrack(List<String> output_array, String currentString, int open, int close, int max){
+    public static void backtrack(List<String> output_array, StringBuilder currentString, int open, int close, int max){
         if(currentString.length() == 2 * max) {
-            output_array.add(currentString);
+            output_array.add(currentString.toString());
             return;
         }
-        if (open<max) backtrack(output_array,currentString+ "(",open+1,close,max);
-        if (close<open) backtrack(output_array,currentString+")",open,close+1,max);
+        if (open<max){
+            backtrack(output_array,currentString.append("("),open+1,close,max);
+            currentString.deleteCharAt(currentString.length()-1);
+        }
+        if (close<open){
+            backtrack(output_array,currentString.append(")"),open,close+1,max);
+            currentString.deleteCharAt(currentString.length()-1);
+        }
     }
 
 }
