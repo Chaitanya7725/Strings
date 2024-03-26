d4c88bde154ebf5605aac9b8b319f16b2d0c2acf
Chaitanya7725 <chaitanya7725@gmail.com>
2024-03-26 14:03:00 -0500

    Used map to store the count and get the max  value, and minus it from the range to get the longest

---
+++ medium/LongestRepeatingCharacterReplacement424.java
@@ -0,0 +1,28 @@
+package medium;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestRepeatingCharacterReplacement424 {
+
+    public static void main(String[] args) {
+        System.out.println(characterReplacement("ABABBA", 2));
+    }
+
+    public static int characterReplacement(String s, int k) {
+        Map<Character,Integer> map = new HashMap<>();
+        int longest =0;
+        int left= 0;
+        for (int right = 0; right < s.length(); right++) {
+            map.put(s.charAt(right),map.getOrDefault(s.charAt(right),0)+1);
+            while((right -left +1)- Collections.max(map.values()) > k ){
+                map.put(s.charAt(right),map.get(s.charAt(right))-1);
+                left+=1;
+            }
+            longest = Math.max(longest,right-left+1);
+        }
+        return longest;
+    }
+
+}
